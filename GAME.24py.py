bbdb289ccf741a851d8bbea921308e7ce30ad1d0
DenysPavlios <denys.pavlios@gmail.com>
2022-08-23 23:03:55 +0300

    game

--- GAME.24py.py
+++ GAME.24py.py
@@ -1,19 +1,24 @@
 import random
 randomInt = random.randint(1, 100)
-print(randomInt)
+print(randomInt) # for exaple
 i = 1
 while True:
     usertype = int(input('Take a guess number from 1 to 100  '))
     if usertype == randomInt:
         print('You WIN!!!, You the best!!!')
         print(f'You tried {i}')
-        break
+        if input(f'Maybe will play again?') == 'y':
+            randomInt = random.randint(1, 100)
+            i = 0
+        else:
+            print('Thanks for game')
+            break
     elif usertype < randomInt:
         print(f'You need still {randomInt - usertype} your number is less')
         print('Will try still')
         print(f'Try # {i}')
         i += 1
-    elif usertype > randomInt:
+    else:
         print(f'You need still {usertype - randomInt} your number is more')
         print(f'Will try still')
         print(f'Try # {i}')
